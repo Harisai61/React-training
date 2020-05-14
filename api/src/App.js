1b846430d0c526adad79dd7bbcab040780b6252c
Harisai61 <65167507+Harisai61@users.noreply.github.com>
2020-05-14 16:43:43 +0530

    Add files via upload

---
+++ api/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import List from './Components/List'
+import Postget from './Postget';
+
+
+function App() {
+  return (
+    <div className="App">
+     {/*<List />*/}
+     <Postget />
+    </div>
+  );
+}
+
+export default App;
